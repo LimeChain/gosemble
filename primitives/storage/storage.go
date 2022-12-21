364587cce82169dcead1dcebf0bf5324577fe3ec
failfmi <oscurocalma@gmail.com>
2022-12-21 10:25:10 +0200

    style: funcs order

--- primitives/storage/storage.go
+++ primitives/storage/storage.go
@@ -7,9 +7,24 @@ import (
 
 func ChangesRoot(parent_hash int64) int64 { panic("Not implemented!") }
 
+func Clear(key []byte) {
+	keyOffsetSize := utils.BytesToOffsetAndSize(key)
+	env.ExtStorageClearVersion1(keyOffsetSize)
+}
+
 func ClearPrefix(key []byte) {
 	keyOffsetSize := utils.BytesToOffsetAndSize(key)
 	env.ExtStorageClearPrefixVersion1(keyOffsetSize)
+}
+
+func Exists(key int64) int32 { panic("Not implemented!") }
+
+func Get(key []byte) []byte {
+	psKey := utils.BytesToOffsetAndSize(key)
+	psValue := env.ExtStorageGetVersion1(psKey)
+	offset, size := utils.Int64ToOffsetAndSize(psValue)
+	value := utils.ToWasmMemorySlice(offset, size)
+	return value
 }
 
 func NextKey(key int64) int64 { panic("Not implemented!") }
@@ -18,23 +33,8 @@ func Read(key int64, value_out int64, offset int32) int64 { panic("Not implement
 
 func Root() int64 { panic("Not implemented!") }
 
-func Exists(key int64) int32 { panic("Not implemented!") }
-
-func Clear(key []byte) {
-	keyOffsetSize := utils.BytesToOffsetAndSize(key)
-	env.ExtStorageClearVersion1(keyOffsetSize)
-}
-
 func Set(key []byte, value []byte) {
 	keyOffsetSize := utils.BytesToOffsetAndSize(key)
 	valueOffsetSize := utils.BytesToOffsetAndSize(value)
 	env.ExtStorageSetVersion1(keyOffsetSize, valueOffsetSize)
-}
-
-func Get(key []byte) []byte {
-	psKey := utils.BytesToOffsetAndSize(key)
-	psValue := env.ExtStorageGetVersion1(psKey)
-	offset, size := utils.Int64ToOffsetAndSize(psValue)
-	value := utils.ToWasmMemorySlice(offset, size)
-	return value
 }
