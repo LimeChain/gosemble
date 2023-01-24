526d14c2bfd26fa558fa71650e04c2497d5ae931
failfmi <oscurocalma@gmail.com>
2023-01-24 09:48:59 +0200

    fix: switch limit to option

--- frame/executive/executive.go
+++ frame/executive/executive.go
@@ -35,7 +35,12 @@ func resetEvents() {
 
 	storage.Clear(append(systemHash, eventsHash...))
 	storage.Clear(append(systemHash, eventCountHash...))
-	storage.ClearPrefix(append(systemHash, eventTopicHash...), sc.U32(math.MaxUint32).Bytes())
+
+	limit := sc.Option[sc.U32]{
+		HasValue: true,
+		Value:    sc.U32(math.MaxUint32),
+	}
+	storage.ClearPrefix(append(systemHash, eventTopicHash...), limit.Bytes())
 }
 
 func runtimeUpgrade() bool {
