b1d5f43dfe288d7c0e5e6a17a151937091409651
Rado M <radkomih@gmail.com>
2023-01-30 20:00:11 +0200

    Refactor nonwasm implementation

---
+++ primitives/storage/storage_nonwasm.go
@@ -0,0 +1,39 @@
+//go:build nonwasmenv
+
+package storage
+
+func ChangesRoot(parent_hash int64) int64 {
+	panic("not implemented")
+}
+
+func Clear(key []byte) {
+	panic("not implemented")
+}
+
+func ClearPrefix(key []byte, limit []byte) {
+	panic("not implemented")
+}
+
+func Exists(key int64) int32 {
+	panic("not implemented")
+}
+
+func Get(key []byte) []byte {
+	panic("not implemented")
+}
+
+func NextKey(key int64) int64 {
+	panic("not implemented")
+}
+
+func Read(key int64, value_out int64, offset int32) int64 {
+	panic("not implemented")
+}
+
+func Root(key []byte) []byte {
+	panic("not implemented")
+}
+
+func Set(key []byte, value []byte) {
+	panic("not implemented")
+}
