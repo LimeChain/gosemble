0368c7007c65964121aa4effcded5cc95db0f8c2
Rado M <radkomih@gmail.com>
2023-02-24 15:47:50 +0200

    add partial weight validation. tests

--- primitives/types/weight.go
+++ primitives/types/weight.go
@@ -30,10 +30,32 @@ func (w Weight) Bytes() []byte {
 	return sc.EncodedBytes(w)
 }
 
+func (w Weight) Sub(otherW Weight) Weight {
+	return Weight{
+		RefTime:   w.RefTime.Sub(otherW.RefTime),
+		ProofSize: w.ProofSize.Sub(otherW.ProofSize),
+	}
+}
+
+func (w Weight) AnyGt(otherW Weight) sc.Bool {
+	// TODO: check if this is correct
+	return w.RefTime > otherW.RefTime // || w.ProofSize > otherW.ProofSize
+}
+
+// Construct [`Weight`] from weight parts, namely reference time and proof size weights.
+func WeightFromParts(refTime sc.U64, proofSize sc.U64) Weight {
+	return Weight{refTime, proofSize}
+}
+
 // Construct [`Weight`] with reference time weight and 0 storage size weight.
 func WeightFromRefTime(refTime sc.U64) Weight {
 	return Weight{
 		RefTime:   refTime,
 		ProofSize: 0,
 	}
+}
+
+// Return a [`Weight`] where all fields are zero.
+func WeightZero() Weight {
+	return Weight{RefTime: 0, ProofSize: 0}
 }
