671037727e14d10d2f3d5b9dd0d5f9e73dd60010
failfmi <oscurocalma@gmail.com>
2023-07-27 11:20:30 +0300

    nit: missing test file change

--- runtime/transactional_test.go
+++ runtime/transactional_test.go
@@ -4,8 +4,10 @@ import (
 	"testing"
 
 	sc "github.com/LimeChain/goscale"
+	"github.com/LimeChain/gosemble/config"
 	"github.com/LimeChain/gosemble/execution/types"
 	"github.com/LimeChain/gosemble/frame/testable/dispatchables"
+	"github.com/LimeChain/gosemble/frame/testable/module"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -16,7 +18,7 @@ import (
 func Test_Storage_Layer_Rollback_Then_Commit(t *testing.T) {
 	rt, storage := newTestRuntime(t)
 
-	call := dispatchables.NewTestCall(sc.NewVaryingData(sc.Sequence[sc.U8]{}))
+	call := dispatchables.NewTestCall(config.TestableIndex, module.FunctionTestIndex, sc.NewVaryingData(sc.Sequence[sc.U8]{}))
 
 	extrinsic := types.NewUnsignedUncheckedExtrinsic(call)
 
