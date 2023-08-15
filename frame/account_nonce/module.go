6f3c40d754b3927cd8142400c6427e42b536177d
failfmi <oscurocalma@gmail.com>
2023-08-15 09:16:46 +0300

    refactor(api): account nonce (#159)
    * refactor(api): account nonce
    
    * refactor(tx-payments): use multiplier config const

--- frame/account_nonce/module.go
+++ frame/account_nonce/module.go
@@ -3,10 +3,18 @@ package account_nonce
 import (
 	"bytes"
 
-	"github.com/LimeChain/gosemble/frame/system"
+	"github.com/LimeChain/gosemble/frame/system/module"
 	"github.com/LimeChain/gosemble/primitives/types"
 	"github.com/LimeChain/gosemble/utils"
 )
+
+type Module struct {
+	systemModule module.SystemModule
+}
+
+func New(systemModule module.SystemModule) Module {
+	return Module{systemModule}
+}
 
 // AccountNonce returns the account nonce of given AccountId.
 // It takes two arguments:
@@ -15,12 +23,12 @@ import (
 // which represent the SCALE-encoded AccountId.
 // Returns a pointer-size of the SCALE-encoded nonce of the AccountId.
 // [Specification](https://spec.polkadot.network/chap-runtime-api#sect-accountnonceapi-account-nonce)
-func AccountNonce(dataPtr int32, dataLen int32) int64 {
+func (m Module) AccountNonce(dataPtr int32, dataLen int32) int64 {
 	b := utils.ToWasmMemorySlice(dataPtr, dataLen)
 	buffer := bytes.NewBuffer(b)
 
 	publicKey := types.DecodePublicKey(buffer)
-	nonce := system.StorageGetAccount(publicKey).Nonce
+	nonce := m.systemModule.Get(publicKey).Nonce
 
 	return utils.BytesToOffsetAndSize(nonce.Bytes())
 }
