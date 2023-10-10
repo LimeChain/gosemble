655c4d0e39d144cac3e152cbceca13937f17ca62
failfmi <oscurocalma@gmail.com>
2023-10-10 12:31:10 +0300

    test(api): account nonce

---
+++ api/account_nonce/module_test.go
@@ -0,0 +1,66 @@
+package account_nonce
+
+import (
+	"testing"
+
+	"github.com/ChainSafe/gossamer/lib/common"
+	sc "github.com/LimeChain/goscale"
+	"github.com/LimeChain/gosemble/constants"
+	"github.com/LimeChain/gosemble/mocks"
+	"github.com/LimeChain/gosemble/primitives/types"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	mockSystem      *mocks.SystemModule
+	mockMemoryUtils *mocks.MemoryTranslator
+)
+
+func Test_Module_Name(t *testing.T) {
+	target := setup()
+
+	assert.Equal(t, ApiModuleName, target.Name())
+}
+
+func Test_Module_Item(t *testing.T) {
+	target := setup()
+
+	hexName := common.MustBlake2b8([]byte(ApiModuleName))
+	expect := types.NewApiItem(hexName[:], apiVersion)
+
+	result := target.Item()
+
+	assert.Equal(t, expect, result)
+}
+
+func Test_Module_AccountNonce(t *testing.T) {
+	target := setup()
+
+	publicKey := constants.OneAddress.FixedSequence
+	nonce := sc.U32(5)
+	accountInfo := types.AccountInfo{
+		Nonce: nonce,
+	}
+	expect := int64(7)
+
+	mockMemoryUtils.On("GetWasmMemorySlice", int32(0), int32(1)).Return(publicKey.Bytes())
+	mockSystem.On("Get", publicKey).Return(accountInfo)
+	mockMemoryUtils.On("BytesToOffsetAndSize", nonce.Bytes()).Return(expect)
+
+	result := target.AccountNonce(0, 1)
+
+	assert.Equal(t, expect, result)
+	mockMemoryUtils.AssertCalled(t, "GetWasmMemorySlice", int32(0), int32(1))
+	mockSystem.AssertCalled(t, "Get", publicKey)
+	mockMemoryUtils.AssertCalled(t, "BytesToOffsetAndSize", nonce.Bytes())
+}
+
+func setup() Module {
+	mockSystem = new(mocks.SystemModule)
+	mockMemoryUtils = new(mocks.MemoryTranslator)
+
+	target := New(mockSystem)
+	target.memUtils = mockMemoryUtils
+
+	return target
+}
