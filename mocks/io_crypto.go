1a7200f45c10234066839659bb5b403b9ab288de
failfmi <oscurocalma@gmail.com>
2023-10-10 18:04:38 +0300

    test(api): session keys

---
+++ mocks/io_crypto.go
@@ -0,0 +1,36 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+type IoCrypto struct {
+	mock.Mock
+}
+
+func (m *IoCrypto) EcdsaGenerate(keyTypeId []byte, seed []byte) []byte {
+	args := m.Called(keyTypeId, seed)
+	return args.Get(0).([]byte)
+}
+
+func (m *IoCrypto) Ed25519Generate(keyTypeId []byte, seed []byte) []byte {
+	args := m.Called(keyTypeId, seed)
+
+	return args.Get(0).([]byte)
+}
+
+func (m *IoCrypto) Ed25519Verify(signature []byte, message []byte, pubKey []byte) bool {
+	args := m.Called(signature, message, pubKey)
+
+	return args.Get(0).(bool)
+}
+
+func (m *IoCrypto) Sr25519Generate(keyTypeId []byte, seed []byte) []byte {
+	args := m.Called(keyTypeId, seed)
+
+	return args.Get(0).([]byte)
+}
+
+func (m *IoCrypto) Sr25519Verify(signature []byte, message []byte, pubKey []byte) bool {
+	args := m.Called(signature, message, pubKey)
+
+	return args.Get(0).(bool)
+}
