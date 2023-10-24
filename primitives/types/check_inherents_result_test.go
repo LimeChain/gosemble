b15a24cedd6f8481fcb0b69b9beaa93fdfb8823e
Radosvet M <radkomih@gmail.com>
2023-10-24 11:55:40 +0300

    test(primitives/types) (#257)
    * test(primitives/types): CheckInherentsResult,InherentError,DigestItem,Digest,DispatchClass,PerDispatchClass
    
    * test example + fix

---
+++ primitives/types/check_inherents_result_test.go
@@ -0,0 +1,123 @@
+package types
+
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+
+	sc "github.com/LimeChain/goscale"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	expectedCheckInherentsResultOkBytes, _  = hex.DecodeString("010000")
+	expectedCheckInherentsResultErrBytes, _ = hex.DecodeString("00010474657374696e67300402")
+	invalidCheckInherentsResultBytes, _     = hex.DecodeString("01000101")
+)
+
+var (
+	inherentIdentifier = [8]byte{'t', 'e', 's', 't', 'i', 'n', 'g', '0'}
+	fatalError         = NewTimestampErrorInvalid()
+
+	inherentDataOk = InherentData{
+		data: map[[8]byte]sc.Sequence[sc.U8]{},
+	}
+
+	inherentDataOkWithIdent = InherentData{
+		data: map[[8]byte]sc.Sequence[sc.U8]{
+			inherentIdentifier: {1},
+		},
+	}
+
+	inherentDataErr = InherentData{
+		data: map[[8]byte]sc.Sequence[sc.U8]{
+			inherentIdentifier: {2},
+		},
+	}
+)
+
+var (
+	targetCheckInherentsResultOk = CheckInherentsResult{
+		Okay:       true,
+		FatalError: false,
+		Errors:     inherentDataOk,
+	}
+
+	targetCheckInherentsResultOkWithIdent = CheckInherentsResult{
+		Okay:       true,
+		FatalError: false,
+		Errors:     inherentDataOkWithIdent,
+	}
+
+	targetCheckInherentsResultErr = CheckInherentsResult{
+		Okay:       false,
+		FatalError: true,
+		Errors:     inherentDataErr,
+	}
+)
+
+func Test_NewCheckInherentsResult(t *testing.T) {
+	assert.Equal(t, targetCheckInherentsResultOk, NewCheckInherentsResult())
+}
+
+func Test_CheckInherentsResult_Encode_Ok(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	targetCheckInherentsResultOk.Encode(buffer)
+
+	assert.Equal(t, expectedCheckInherentsResultOkBytes, buffer.Bytes())
+}
+
+func Test_CheckInherentsResult_Encode_Err(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	targetCheckInherentsResultErr.Encode(buffer)
+
+	assert.Equal(t, expectedCheckInherentsResultErrBytes, buffer.Bytes())
+}
+
+func Test_CheckInherentsResult_Bytes_Ok(t *testing.T) {
+	assert.Equal(t, expectedCheckInherentsResultOkBytes, targetCheckInherentsResultOk.Bytes())
+}
+
+func Test_CheckInherentsResult_Bytes_Err(t *testing.T) {
+	assert.Equal(t, expectedCheckInherentsResultErrBytes, targetCheckInherentsResultErr.Bytes())
+}
+
+func Test_CheckInherentsResult_PutError_AlreadyWithError(t *testing.T) {
+	err := targetCheckInherentsResultErr.PutError(inherentIdentifier, fatalError)
+
+	assert.Equal(t, NewInherentErrorFatalErrorReported(), err)
+}
+
+func Test_CheckInherentsResult_PutError(t *testing.T) {
+	err := targetCheckInherentsResultOk.PutError(inherentIdentifier, fatalError)
+
+	assert.Nil(t, err)
+	assert.Equal(t, sc.Bool(false), targetCheckInherentsResultOk.Okay)
+	assert.Equal(t, sc.Bool(true), targetCheckInherentsResultOk.FatalError)
+	assert.Equal(t, inherentDataErr, targetCheckInherentsResultOk.Errors)
+}
+
+func Test_CheckInherentsResult_PutError_ExistingIdentifier(t *testing.T) {
+	err := targetCheckInherentsResultOkWithIdent.PutError(inherentIdentifier, NewInherentErrorApplication())
+
+	assert.Equal(t, NewInherentErrorInherentDataExists(sc.BytesToSequenceU8(inherentIdentifier[:])), err)
+}
+
+func Test_DecodeCheckInherentsResult(t *testing.T) {
+	buffer := bytes.NewBuffer(expectedCheckInherentsResultErrBytes)
+
+	result := DecodeCheckInherentsResult(buffer)
+
+	assert.Equal(t, targetCheckInherentsResultErr, result)
+}
+
+func Test_DecodeCheckInherentsResult_Panics(t *testing.T) {
+	buffer := bytes.NewBuffer(invalidCheckInherentsResultBytes)
+
+	assert.PanicsWithValue(t, "failed to decode InherentData", func() {
+		DecodeCheckInherentsResult(buffer)
+	})
+
+}
---
+++ primitives/types/per_dispatch_class_test.go
@@ -0,0 +1,54 @@
+package types
+
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+
+	sc "github.com/LimeChain/goscale"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	expectedPerDispatchClassBytes, _ = hex.DecodeString("010203")
+
+	targetPerDispatchClass = PerDispatchClass[sc.U8]{
+		Normal:      sc.U8(1),
+		Operational: sc.U8(2),
+		Mandatory:   sc.U8(3),
+	}
+)
+
+func Test_PerDispatchClass_Encode(t *testing.T) {
+	buf := &bytes.Buffer{}
+
+	targetPerDispatchClass.Encode(buf)
+
+	assert.Equal(t, expectedPerDispatchClassBytes, buf.Bytes())
+}
+
+func Test_DecodePerDispatchClass(t *testing.T) {
+	buf := bytes.NewBuffer(expectedPerDispatchClassBytes)
+
+	result := DecodePerDispatchClass(buf, sc.DecodeU8)
+
+	assert.Equal(t, targetPerDispatchClass, result)
+}
+
+func Test_PerDispatchClass_Bytes(t *testing.T) {
+	assert.Equal(t, expectedPerDispatchClassBytes, targetPerDispatchClass.Bytes())
+}
+
+func Test_PerDispatchClass_Get(t *testing.T) {
+	assert.Equal(t, sc.U8(1), *targetPerDispatchClass.Get(NewDispatchClassNormal()))
+	assert.Equal(t, sc.U8(2), *targetPerDispatchClass.Get(NewDispatchClassOperational()))
+	assert.Equal(t, sc.U8(3), *targetPerDispatchClass.Get(NewDispatchClassMandatory()))
+}
+
+func Test_PerDispatchClass_Get_Panic(t *testing.T) {
+	unknownDispatchClass := DispatchClass{sc.NewVaryingData(sc.U8(3))}
+
+	assert.PanicsWithValue(t, "invalid DispatchClass type", func() {
+		targetPerDispatchClass.Get(unknownDispatchClass)
+	})
+}
