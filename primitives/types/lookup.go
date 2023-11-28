ffd944209578c4f48b541e2683dc08bfb7a7f0a9
failfmi <oscurocalma@gmail.com>
2023-11-28 15:17:33 +0200

    fix: lookup

--- primitives/types/lookup.go
+++ primitives/types/lookup.go
@@ -1,42 +1,9 @@
 package types
 
-import sc "github.com/LimeChain/goscale"
-
 func Lookup(a MultiAddress) (AccountId, error) {
-	address, err := lookupAddress(a)
-	if err != nil {
-		return AccountId{}, err
-	}
-	if address.HasValue {
-		return address.Value, nil
+	if !a.IsAccountId() {
+		return AccountId{}, NewTransactionValidityError(NewUnknownTransactionCannotLookup())
 	}
 
-	return AccountId{}, NewTransactionValidityError(NewUnknownTransactionCannotLookup())
-}
-
-// LookupAddress Lookup an address to get an Id, if there's one there.
-func lookupAddress(a MultiAddress) (sc.Option[AccountId], error) {
-	if a.IsAccountId() {
-		accountId, err := a.AsAccountId()
-		if err != nil {
-			return sc.NewOption[AccountId](nil), err
-		}
-		return sc.NewOption[AccountId](accountId), nil
-	}
-
-	if a.IsAccountIndex() {
-		index, err := a.AsAccountIndex()
-		if err != nil {
-			return sc.NewOption[AccountId](nil), err
-		}
-		return lookupIndex(index), nil
-	}
-
-	return sc.NewOption[AccountId](nil), nil
-}
-
-// LookupIndex Lookup an T::AccountIndex to get an Id, if there's one there.
-func lookupIndex(index AccountIndex) sc.Option[AccountId] {
-	// TODO:
-	return sc.NewOption[AccountId](nil)
+	return a.AsAccountId()
 }
