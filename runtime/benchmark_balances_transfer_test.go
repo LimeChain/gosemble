1ef1a00e83ab4f37525257b12cad3aabc8f2998a
Tugay Emin <tgntrr@gmail.com>
2024-01-23 18:09:17 +0200

    refactor benchmark_balances_transfer_test.go

--- runtime/benchmark_balances_transfer_test.go
+++ runtime/benchmark_balances_transfer_test.go
@@ -5,9 +5,8 @@ import (
 	"testing"
 
 	gossamertypes "github.com/ChainSafe/gossamer/dot/types"
-	"github.com/ChainSafe/gossamer/lib/runtime"
-	wazero_runtime "github.com/ChainSafe/gossamer/lib/runtime/wazero"
 	"github.com/ChainSafe/gossamer/pkg/scale"
+	"github.com/LimeChain/gosemble/benchmarking"
 	"github.com/LimeChain/gosemble/primitives/types"
 	ctypes "github.com/centrifuge/go-substrate-rpc-client/v4/types"
 	"github.com/stretchr/testify/assert"
@@ -17,46 +16,44 @@ import (
 // * Transfer will kill the sender account.
 // * Transfer will create the recipient account.
 func BenchmarkBalancesTransferAllowDeath(b *testing.B) {
-	benchmarkInstance(b, BalancesTransfer)
-}
+	benchmarking.Run(b, func(i *benchmarking.Instance) {
+		// arrange
+		balance := existentialMultiplier * existentialAmount
+		transferAmount := existentialAmount*(existentialMultiplier-1) + 1
 
-func BalancesTransfer(b *testing.B, rt *wazero_runtime.Instance, storage *runtime.Storage, metadata *ctypes.Metadata, args ...interface{}) (ctypes.Call, []byte) {
-	// Setup the input params
-	balance := existentialMultiplier * existentialAmount
-	transferAmount := existentialAmount*(existentialMultiplier-1) + 1
+		accountInfo := gossamertypes.AccountInfo{
+			Nonce:       0,
+			Consumers:   0,
+			Producers:   0,
+			Sufficients: 0,
+			Data: gossamertypes.AccountData{
+				Free:       scale.MustNewUint128(big.NewInt(balance)),
+				Reserved:   scale.MustNewUint128(big.NewInt(0)),
+				MiscFrozen: scale.MustNewUint128(big.NewInt(0)),
+				FreeFrozen: scale.MustNewUint128(big.NewInt(0)),
+			},
+		}
 
-	call, err := ctypes.NewCall(metadata, "Balances.transfer", bobAddress, ctypes.NewUCompact(big.NewInt(transferAmount)))
-	assert.NoError(b, err)
+		err := i.SetAccountInfo(aliceAccountIdBytes, accountInfo)
+		assert.NoError(b, err)
 
-	benchmarkConfig := newExtrinsicCall(b, types.NewRawOriginSigned(aliceAccountId), call)
+		// act
+		err = i.ExecuteExtrinsic(
+			"Balances.transfer",
+			types.NewRawOriginSigned(aliceAccountId),
+			bobAddress,
+			ctypes.NewUCompact(big.NewInt(transferAmount)),
+		)
 
-	// Setup the state
-	aliceAccountInfo := gossamertypes.AccountInfo{
-		Nonce:       0,
-		Consumers:   0,
-		Producers:   0,
-		Sufficients: 0,
-		Data: gossamertypes.AccountData{
-			Free:       scale.MustNewUint128(big.NewInt(balance)),
-			Reserved:   scale.MustNewUint128(big.NewInt(0)),
-			MiscFrozen: scale.MustNewUint128(big.NewInt(0)),
-			FreeFrozen: scale.MustNewUint128(big.NewInt(0)),
-		},
-	}
-	setAccountInfo(b, storage, aliceAccountIdBytes, aliceAccountInfo)
+		// assert
+		assert.NoError(b, err)
 
-	aliceInfo := getAccountInfo(b, storage, aliceAccountIdBytes)
-	assert.Equal(b, scale.MustNewUint128(big.NewInt(balance)), aliceInfo.Data.Free)
+		senderInfo, err := i.GetAccountInfo(aliceAccountIdBytes)
+		assert.NoError(b, err)
+		assert.Equal(b, scale.MustNewUint128(big.NewInt(balance-int64(transferAmount))), senderInfo.Data.Free)
 
-	// Execute the call
-	res, err := rt.Exec("Benchmark_run", benchmarkConfig.Bytes())
-	assert.NoError(b, err)
-
-	// Validate the result
-	aliceInfo = getAccountInfo(b, storage, aliceAccountIdBytes)
-	assert.Equal(b, scale.MustNewUint128(big.NewInt(balance-int64(transferAmount))), aliceInfo.Data.Free)
-	bobInfo := getAccountInfo(b, storage, bobAccountIdBytes)
-	assert.Equal(b, scale.MustNewUint128(big.NewInt(int64(transferAmount))), bobInfo.Data.Free)
-
-	return call, res
+		recipientInfo, err := i.GetAccountInfo(bobAccountIdBytes)
+		assert.NoError(b, err)
+		assert.Equal(b, scale.MustNewUint128(big.NewInt(int64(transferAmount))), recipientInfo.Data.Free)
+	})
 }
