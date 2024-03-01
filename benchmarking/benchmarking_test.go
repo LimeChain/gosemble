efadf282dde40ccb8dd9a973275274e7995021ec
Tugay Emin <tgntrr@gmail.com>
2024-03-01 09:53:41 +0200

    fix test

--- benchmarking/benchmarking_test.go
+++ benchmarking/benchmarking_test.go
@@ -25,7 +25,6 @@ func TestRun(t *testing.T) {
 	outputPath := filepath.Join(t.TempDir(), "output.go")
 
 	// set benchmarking flags
-	os.Args = append(os.Args, "-steps", "3")
 	os.Args = append(os.Args, "-test.benchtime", "1x")
 	flag.Parse()
 
@@ -41,7 +40,7 @@ func TestRun(t *testing.T) {
 			componentValues = append(componentValues, component.Value())
 		}, component)
 
-		assert.Equal(t, []uint32{1, 50, 100}, componentValues)
+		assert.Equal(t, []uint32{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99}, componentValues)
 	})
 }
 
